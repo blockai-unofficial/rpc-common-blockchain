89cdb9569d7ec5fbbb471cc20656929afc9637b1
William Cotton <williamcotton@gmail.com>
2015-10-08 11:28:18 -0700

    v1.3.0 - added Unpents

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
-module.exports = function(options) {
+module.exports = function (options) {
+  var rpc = options.rpc
 
-  var rpc = options.rpc;
+  var txHexToJSON = require('bitcoin-tx-hex-to-json')
+  var bitcoin = require('bitcoinjs-lib')
+  var txToJson = require('./tx-to-json')
 
-  var txHexToJSON = require('bitcoin-tx-hex-to-json');
-  var bitcoin = require('bitcoinjs-lib'); 
-  var txToJson = require('./tx-to-json');
-
-  var GetTranasactions = function(txids, callback) {
-
-    function batchCall() {
+  var GetTranasactions = function (txids, callback) {
+    function batchCall () {
       txids.forEach(function (txid) {
-        rpc.getRawTransaction(txid);
-      });
+        rpc.getRawTransaction(txid)
+      })
     }
 
-    rpc.batch(batchCall, function(err, rawtxs) {
+    rpc.batch(batchCall, function (err, rawtxs) {
       var txs = rawtxs.map(function (rawtx) {
-        return rawtx.result ? txHexToJSON(rawtx.result) : false;
-      });
-      callback(err, txs);
-    });
-
+        return rawtx.result ? txHexToJSON(rawtx.result) : false
+      })
+      callback(err, txs)
+    })
   }
 
-  var Latest = function(callback) {
-    callback(false, []);
-  };
+  var Latest = function (callback) {
+    callback(false, [])
+  }
 
-  var Outputs = function(txidvouts, callback) {
-
-    function batchCall() {
+  var Outputs = function (txidvouts, callback) {
+    function batchCall () {
       txidvouts.forEach(function (txidvout) {
-        rpc.getRawTransaction(txidvout.txid);
-      });
+        rpc.getRawTransaction(txidvout.txid)
+      })
     }
 
-    rpc.batch(batchCall, function(err, rawtxs) {
+    rpc.batch(batchCall, function (err, rawtxs) {
       var outputs = rawtxs.map(function (rawTx, i) {
         if (!rawTx.result) {
-          return false;
+          return false
         }
-        var tx = txHexToJSON(rawTx.result);
-        var rawOutput = tx.vout[txidvouts[i].vout];
+        var tx = txHexToJSON(rawTx.result)
+        var rawOutput = tx.vout[txidvouts[i].vout]
         var output = {
           txid: tx.txid,
           txId: tx.txid,
@@ -49,21 +45,19 @@ module.exports = function(options) {
           vout: rawOutput.n,
           scriptPubKey: rawOutput.scriptPubKey.hex
         }
-        return output;
-      });
-      callback(err, outputs);
-    });
+        return output
+      })
+      callback(err, outputs)
+    })
   }
 
-  var Propagate = function(txHex, callback) {
-    rpc.sendRawTransaction(txHex, function(err, res) {
-      callback(err, res);
-    });
+  var Propagate = function (txHex, callback) {
+    rpc.sendRawTransaction(txHex, function (err, res) {
+      callback(err, res)
+    })
   }
 
-  var Status = function() {
-
-  };
+  var Status = function () {}
 
   var Transactions = {
     Get: GetTranasactions,
@@ -73,37 +67,64 @@ module.exports = function(options) {
     Status: Status
   }
 
-  // https://bitcoin.org/en/developer-reference#importaddress
+  var Unspents = function (addresses, callback) {
+    var outputAddressesObject = {}
+    addresses.forEach(function (addr) {
+      outputAddressesObject[addr] = []
+    })
+    rpc.listUnspent(0, 9999999, addresses, function (err, res) {
+      var rawOutputs = res.result
+      rawOutputs.forEach(function (output) {
+        outputAddressesObject[output.address].push({
+          address: output.address,
+          txid: output.txid,
+          txId: output.txid,
+          scriptPubKey: output.scriptPubKey,
+          vout: output.vout,
+          amount: output.amount * 100000000,
+          value: output.amount * 100000000,
+          confirmations: output.confirmations
+        })
+      })
+      var outputAddresses = []
+      for (var address in outputAddressesObject) {
+        outputAddresses.push(outputAddressesObject[address])
+      }
+      callback(err, outputAddresses)
+    })
+  }
+
   var Addresses = {
-
+    Unspents: Unspents
   }
 
-  var GetBlocks = function(blockIds, callback) {
-    var blockId = blockIds[0];
-    rpc.getBlock(blockId, function(err, jsonRes) {
-      var block = jsonRes.result;
-      rpc.getBlock(blockId, false, function(err, rawRes) {
-        var blockHex = rawRes.result;
-        block.blockHex = blockHex;
-        block.blockId = blockId;
+  var GetBlocks = function (blockIds, callback) {
+    var blockId = blockIds[0]
+    rpc.getBlock(blockId, function (err, jsonRes) {
+      if (err) {} // TODO
+      var block = jsonRes.result
+      rpc.getBlock(blockId, false, function (err, rawRes) {
+        var blockHex = rawRes.result
+        block.blockHex = blockHex
+        block.blockId = blockId
 
-        var rawBlock = bitcoin.Block.fromHex(blockHex);
-        var transactions = [];
-        rawBlock.transactions.forEach(function(tx) {
-          var txJSON = txToJson(tx);
-          transactions.push(txJSON);
-        });
-        block.transactions = transactions;
-        callback(err, block);
-      });
-    });
+        var rawBlock = bitcoin.Block.fromHex(blockHex)
+        var transactions = []
+        rawBlock.transactions.forEach(function (tx) {
+          var txJSON = txToJson(tx)
+          transactions.push(txJSON)
+        })
+        block.transactions = transactions
+        callback(err, block)
+      })
+    })
   }
 
-  var GetBlockHash = function(blockHeight, callback) {
-    rpc.getBlockHash(blockHeight, function(err, res) {
-      var blockId = res.result;
-      callback(err, blockId);
-    });
+  var GetBlockHash = function (blockHeight, callback) {
+    rpc.getBlockHash(blockHeight, function (err, res) {
+      var blockId = res.result
+      callback(err, blockId)
+    })
   }
 
   var Blocks = {
@@ -113,7 +134,7 @@ module.exports = function(options) {
 
   return {
     Transactions: Transactions,
-    Blocks: Blocks
+    Blocks: Blocks,
+    Addresses: Addresses
   }
-
 }
--- test.js
+++ test.js
@@ -1,39 +1,34 @@
-var test = require('tape');
-var txHexToJSON = require('bitcoin-tx-hex-to-json');  
-var RpcClient = require('bitcoind-rpc');
-var env = require('node-env-file');
-env('./.env', { raise: false });
+var test = require('tape')
+var RpcClient = require('bitcoind-rpc')
+var env = require('node-env-file')
+env('./.env', { raise: false })
 
-var rpcuser = process.env.rpcuser;
-var rpcpassword = process.env.rpcpassword;
- 
+var rpcuser = process.env.rpcuser
+var rpcpassword = process.env.rpcpassword
+
 var config = {
   protocol: 'http',
   user: rpcuser,
   pass: rpcpassword,
   host: '127.0.0.1',
-  port: '18332',
-};
+  port: '18332'
+}
 
-var rpc = new RpcClient(config);
+var rpc = new RpcClient(config)
 
-var commonBlockchain = require("./")({
+var testnetCommonBlockchainTests = require('abstract-common-blockchain/tests/testnet')
+
+var testnetCommonBlockchain = require('./')({
   rpc: rpc
-});
-
-var testnetCommonBlockchainTests = require('abstract-common-blockchain/tests/testnet');
-
-var testnetCommonBlockchain = require("./")({
-  rpc: rpc
-});
+})
 
 var testnetCommon = {
-  setup: function(t, cb) {
-    cb(null, testnetCommonBlockchain);
+  setup: function (t, cb) {
+    cb(null, testnetCommonBlockchain)
   },
-  teardown: function(t, testnetCommonBlockchain, cb) {
-    cb();
+  teardown: function (t, testnetCommonBlockchain, cb) {
+    cb()
   }
 }
 
-testnetCommonBlockchainTests(test, testnetCommon);
+testnetCommonBlockchainTests(test, testnetCommon)
